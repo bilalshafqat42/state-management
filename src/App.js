3cedfe5254921d8a3ef7255d6695d76d9bb55a0a
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-12-08 05:47:34 +0400

    new udpate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { add, remove, incByFive } from "./store/counterSlice";
 import CounterHeader from "./components/CounterHeader";
 import CounterBody from "./components/CounterBody";
 
 const App = () => {
-  const counter = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
     <div className="main-container">
       <h4>Counter App</h4>
